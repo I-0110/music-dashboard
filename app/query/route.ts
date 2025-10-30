12dd9f60e86a586dbc645535b050cd5128cab8a2
IvelisBecker <ivelisbecker@gmail.com>
2025-10-29 20:55:40 -0500

    Altered lib, query, and seed folders --not tested

--- app/query/route.ts
+++ app/query/route.ts
@@ -2,6 +2,31 @@
 
 // const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
+// async function listVideos() {
+//  const data = await sql`
+//    SELECT videos.name
+//    FROM videos
+//    `;
+//  return data;
+// }
+
+export async function GET() {
+  return Response.json({
+    message: 
+    'Uncomment this file and remove this line. You can delete this file when you are finished.',
+  });
+  // try {
+  // 	return Response.json(await listInvoices());
+  // } catch (error) {
+  // 	return Response.json({ error }, { status: 500 });
+  // }
+}
+
+// Origin:
+// import postgres from 'postgres';
+
+// const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
+
 // async function listInvoices() {
 // 	const data = await sql`
 //     SELECT invoices.amount, customers.name
@@ -13,14 +38,14 @@
 // 	return data;
 // }
 
-export async function GET() {
-  return Response.json({
-    message:
-      'Uncomment this file and remove this line. You can delete this file when you are finished.',
-  });
+// export async function GET() {
+//   return Response.json({
+//     message:
+//       'Uncomment this file and remove this line. You can delete this file when you are finished.',
+//   });
   // try {
   // 	return Response.json(await listInvoices());
   // } catch (error) {
   // 	return Response.json({ error }, { status: 500 });
   // }
-}
+// }
