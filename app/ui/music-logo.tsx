bfe23169579766e58a2cb78be80421d33e4b077d
IvelisBecker <ivelisbecker@gmail.com>
2025-10-31 12:28:33 -0500

    Updated up to Chapter 2 from Dashboard Tutorial

--- app/ui/music-logo.tsx
+++ app/ui/music-logo.tsx
@@ -1,7 +1,7 @@
 import { MusicalNoteIcon, PlayCircleIcon } from '@heroicons/react/24/outline';
 import { teachers } from '@/app/ui/fonts';
 
-export default function AcmeLogo() {
+export default function MusicLogo() {
   return (
     <div
       className={`${teachers.className} flex flex-row items-center leading-none text-white`}
